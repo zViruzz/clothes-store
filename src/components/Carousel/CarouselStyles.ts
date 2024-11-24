18792946c645e95663305980d2111339a8b89ff0
zViruzz <nahuelsinka@gmail.com>
2024-11-24 18:09:17 -0300

    style(footer, header): style update

--- src/components/Carousel/CarouselStyles.ts
+++ src/components/Carousel/CarouselStyles.ts
@@ -6,7 +6,7 @@ const stylesSlots = tv({
 		line: 'bg-primary w-44 h-1 rounded-full',
 		cardContainer: 'overflow-x-scroll relative scrollbar-hide',
 		layoutContainer:
-			'flex gap-10 w-[calc(384px_*_10)] md:px-10 px-4 flex-col md:flex-row animate-carousel whitespace-nowrap [&>li]:h-96 [&>li]:w-96 [&>li]:aspect-square',
+			'flex gap-10 w-[calc(384px_*_10)] md:px-10 px-4 md:flex-row animate-carousel whitespace-nowrap [&>li]:h-96 [&>li]:w-96 [&>li]:aspect-square',
 		subheading:
 			'flex flex-col items-center [&>h2]:text-3xl [&>h2]:text-center my-9 [&>h2]:font-medium',
 	},
